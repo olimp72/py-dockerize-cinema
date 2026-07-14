ed1bd556e8a750be9e35fe464c4ac0ead6394579
Andrey Bilenko <olimp_72@ukr.net>
2026-07-14 22:32:35 +0300

    Fix_Bags_Solution

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -1,22 +1,35 @@
 import time
+import sys
 from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
+    help = "Очікування доки база даних не стане доступною"  # noqa: VNE003
 
     def handle(self, *args, **options):
         self.stdout.write("Очікування бази даних...")
         db_conn = None
-        while not db_conn:
+        max_retries = 30
+        retries = 0
+
+        while not db_conn and retries < max_retries:
             try:
                 db_conn = connections["default"]
                 db_conn.cursor()
             except OperationalError:
+                retries += 1
                 self.stdout.write(
-                    "База даних недоступна, очікування 1 секунду..."
+                    f"База даних недоступна, очікування 1 сек... "
+                    f"(Спроба {retries}/{max_retries})"
                 )
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS("База даних доступна!"))
+        if db_conn:
+            self.stdout.write(self.style.SUCCESS("База даних доступна!"))
+        else:
+            self.stdout.write(
+                self.style.ERROR("Перевищено час очікування БД. Вихід...")
+            )
+            sys.exit(1)
